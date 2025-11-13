5d154b54d596592a2aa5cb4b53d2b74b42211986
NachoProductions <tarek.shinya10@gmail.com>
2025-11-13 09:59:44 -0300

    Objeto Adrenalina - 13/11 - 10:02
    Añadi Objeto de Adrenalina

--- Assets/Scripts/ItemTemplate.cs
+++ Assets/Scripts/ItemTemplate.cs
@@ -8,14 +8,18 @@ public class ItemTemplate : ScriptableObject
     public enum ITEM_TYPE
     {
         None,
-        Key,
-        Can,
+        KeyCard,
         Botiquin,
         Adrenalina
     }
     public ITEM_TYPE itemType;
 
-    [Header("Datos de Consumible")]
+    [Header("Datos de Uso General")]
     public float useDuration;
+
+    [Header("Datos de Botiquín")]
     public float healAmount;
+
+    [Header("Datos de Adrenalina")]
+    public float adrenalineDuration;
 }
