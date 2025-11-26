6f05a48c9de0067b840ee7b1ef64f0165963cdb6
ElChuloPapaya <elchulopapaya@gmail.com>
2025-11-26 19:45:39 -0300

    Creditos Funcionales :b 19:45
    Hice funcionar los creditos dea redesubicadovich insta nashi

--- Assets/Scripts/Interface-Scripts/AnimationInterfaceManager.cs
+++ Assets/Scripts/Interface-Scripts/AnimationInterfaceManager.cs
@@ -4,7 +4,13 @@ using UnityEngine.SceneManagement;
 public class AnimationInterfaceManager : MonoBehaviour
 {
     public Animator anim, anim2, anim3;
+    public GameObject CreditsBack;
 
+
+    private void Start()
+    {
+        CreditsBack.SetActive(false);
+    }
     public void Button()
     {
         anim.SetTrigger("ButtonAnim");
@@ -13,6 +19,14 @@ public class AnimationInterfaceManager : MonoBehaviour
     public void Credits()
     {
         anim3.SetTrigger("CreditAnim");
+        CreditsBack.SetActive(true);
+    }
+
+    public void BackFromCredits()
+    {
+        anim3.SetTrigger("CreditAnim");
+        CreditsBack.SetActive(false);
+
     }
 
     public void Window()
