28d7bb2446a198aa458f97557a87575c44b8df0c
ElChuloPapaya <elchulopapaya@gmail.com>
2025-11-30 19:57:51 -0300

    Adición del Titulo 19:57
    Agregué el titulo pal juego, además de un script nuevo, pa que al apretar los creditos se desaparezca

---
+++ Assets/Scripts/Interface-Scripts/FadeOutUI.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+public class FadeOutUI : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    private CanvasGroup cg;
+
+    void Awake()
+    {
+        cg = GetComponent<CanvasGroup>();
+    }
+
+    public void FadeIn()
+    {
+        gameObject.SetActive(true);
+        StartCoroutine(Fade(0f, 1f));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(Fade(1f, 0f));
+    }
+
+    private System.Collections.IEnumerator Fade(float start, float end)
+    {
+        float t = 0f;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            cg.alpha = Mathf.Lerp(start, end, t / fadeDuration);
+            yield return null;
+        }
+
+        cg.alpha = end;
+
+        if (end == 0f)
+            gameObject.SetActive(false);
+    }
+}
